517792b4bfe862e47eb96f43efa303d15c358c13
Ch4var <schavarriac2003@gmail.com>
2025-04-08 17:38:52 -0600

    Agregado validaciones de datos y funcion de buscar a beneficiarios

--- apaman-backend/src/main/java/com/apaman/apaman_backend/repository/BeneficiarioRepository.java
+++ apaman-backend/src/main/java/com/apaman/apaman_backend/repository/BeneficiarioRepository.java
@@ -2,6 +2,16 @@ package com.apaman.apaman_backend.repository;
 
 import com.apaman.apaman_backend.model.Beneficiario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 
 public interface BeneficiarioRepository extends JpaRepository<Beneficiario, Long> {
+    @Query("SELECT b FROM Beneficiario b WHERE CONCAT('', b.cedula) LIKE %:cedula%")
+    List<Beneficiario> findByCedulaContaining(@Param("cedula") String cedula);
+
+    @Query("SELECT b FROM Beneficiario b WHERE LOWER(b.nombre) LIKE LOWER(CONCAT('%', :nombre, '%'))")
+    List<Beneficiario> findByNombreContainingIgnoreCase(@Param("nombre") String nombre);
 }
