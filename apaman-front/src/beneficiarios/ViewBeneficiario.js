ad97d85af4a1f2565863cecbf0c2b764d6f27b0d
Ch4var <schavarriac2003@gmail.com>
2025-04-07 19:17:28 -0600

    Cambio a ViewBeneficiario para que se muestre la foto

--- apaman-front/src/beneficiarios/ViewBeneficiario.js
+++ apaman-front/src/beneficiarios/ViewBeneficiario.js
@@ -29,24 +29,92 @@ export default function ViewBeneficiario() {
             <h2>Detalles del Beneficiario</h2>
             <table className="table table-bordered mt-3">
                 <tbody>
-                <tr><th>Cédula</th><td>{beneficiario.cedula}</td></tr>
-                <tr><th>Nombre</th><td>{beneficiario.nombre}</td></tr>
-                <tr><th>Sexo</th><td>{beneficiario.sexo}</td></tr>
-                <tr><th>Fecha Nacimiento</th><td>{beneficiario.fechaNacimiento}</td></tr>
-                <tr><th>Edad</th><td>{beneficiario.edad}</td></tr>
-                <tr><th>Religión</th><td>{beneficiario.religion}</td></tr>
-                <tr><th>Escolaridad</th><td>{beneficiario.gradoEscolaridad}</td></tr>
-                <tr><th>Dependencia</th><td>{beneficiario.estadoDependencia}</td></tr>
-                <tr><th>Ingreso</th><td>{beneficiario.fechaIngreso}</td></tr>
-                <tr><th>Estado</th><td>{beneficiario.estado}</td></tr>
-                <tr><th>Contacto</th><td>{beneficiario.infoContacto}</td></tr>
-                <tr><th>Responsable</th><td>{beneficiario.personaResponsable}</td></tr>
-                <tr><th>Tel. Responsable</th><td>{beneficiario.telefonoResponsable}</td></tr>
-                <tr><th>Dirección Responsable</th><td>{beneficiario.direccionResponsable}</td></tr>
-                <tr><th>Info Financiera</th><td>{beneficiario.infoFinanciera}</td></tr>
-                <tr><th>Pensionado</th><td>{beneficiario.pensionado ? "Sí" : "No"}</td></tr>
-                <tr><th>Presupuesto</th><td>{beneficiario.presupuesto}</td></tr>
-                <tr><th>Observaciones</th><td>{beneficiario.observaciones}</td></tr>
+                <tr>
+                    <th>Cédula</th>
+                    <td>{beneficiario.cedula}</td>
+                </tr>
+                <tr>
+                    <th>Nombre</th>
+                    <td>{beneficiario.nombre}</td>
+                </tr>
+                <tr>
+                    <th>Sexo</th>
+                    <td>{beneficiario.sexo}</td>
+                </tr>
+                <tr>
+                    <th>Fecha Nacimiento</th>
+                    <td>{beneficiario.fechaNacimiento}</td>
+                </tr>
+                <tr>
+                    <th>Edad</th>
+                    <td>{beneficiario.edad}</td>
+                </tr>
+                <tr>
+                    <th>Religión</th>
+                    <td>{beneficiario.religion}</td>
+                </tr>
+                <tr>
+                    <th>Escolaridad</th>
+                    <td>{beneficiario.gradoEscolaridad}</td>
+                </tr>
+                <tr>
+                    <th>Dependencia</th>
+                    <td>{beneficiario.estadoDependencia}</td>
+                </tr>
+                <tr>
+                    <th>Ingreso</th>
+                    <td>{beneficiario.fechaIngreso}</td>
+                </tr>
+                <tr>
+                    <th>Estado</th>
+                    <td>{beneficiario.estado}</td>
+                </tr>
+                <tr>
+                    <th>Contacto</th>
+                    <td>{beneficiario.infoContacto}</td>
+                </tr>
+                <tr>
+                    <th>Responsable</th>
+                    <td>{beneficiario.personaResponsable}</td>
+                </tr>
+                <tr>
+                    <th>Tel. Responsable</th>
+                    <td>{beneficiario.telefonoResponsable}</td>
+                </tr>
+                <tr>
+                    <th>Dirección Responsable</th>
+                    <td>{beneficiario.direccionResponsable}</td>
+                </tr>
+                <tr>
+                    <th>Info Financiera</th>
+                    <td>{beneficiario.infoFinanciera}</td>
+                </tr>
+                <tr>
+                    <th>Pensionado</th>
+                    <td>{beneficiario.pensionado ? "Sí" : "No"}</td>
+                </tr>
+                <tr>
+                    <th>Presupuesto</th>
+                    <td>{beneficiario.presupuesto}</td>
+                </tr>
+                <tr>
+                    <th>Observaciones</th>
+                    <td>{beneficiario.observaciones}</td>
+                </tr>
+                <tr>
+                    <th>Foto</th>
+                    <td>
+                        {beneficiario.foto ? (
+                            <img
+                                src={`data:image/jpeg;base64,${beneficiario.foto}`}
+                                alt="Foto del beneficiario"
+                                style={{ width: '200px', height: 'auto' }}
+                            />
+                        ) : (
+                            "Sin foto"
+                        )}
+                    </td>
+                </tr>
                 </tbody>
             </table>
             <button className="btn btn-secondary mt-3" onClick={() => navigate(-1)}>Volver</button>
